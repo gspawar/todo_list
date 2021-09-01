cd9d5c26462fec7dec80db9f44ce3e7eaaaff6d1
ganeshpawar <gspawar@mitaoe.ac.in>
2021-09-01 13:58:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState}from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ListGroup,Button} from 'react-bootstrap';
 
 function App() {
+
+  const[InputList, setInputList]=useState("");
+  const[Item, setItem]=useState([]);
+
+  const itemEvent=(event)=>{
+    setInputList(event.target.value)
+  };
+
+  const ListItem =()=>{
+    setItem((oldI)=>{
+      return [...oldI, InputList];
+
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+      <h1>TODO App </h1>
+      <br/>
+      <input  type="text" placeholder="Add Items" onChange={itemEvent} ></input>
+      <Button variant="info" onClick={ListItem}> Add </Button>
+      
+
+      <ol>
+        
+        {Item.map((items)=>{
+        return <ListGroup variant="success">{items}</ListGroup>;
+        
+         } )}
+      </ol>
+
     </div>
   );
 }
